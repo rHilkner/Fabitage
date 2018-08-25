9abf7b3110a676ae71794c4cdbf5b26dd9d70a2f
Rodrigo Hilkner <rodrigo.hilkner@gmail.com>
2018-08-25 15:54:42 -0300

    vaiqvai

--- FabiDoctor/FabiDoctor/FabiDoctor/Objects/User.swift
+++ FabiDoctor/FabiDoctor/FabiDoctor/Objects/User.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-class User: Codable {
+class User {
 
     var name: String?
     var birthDate: Date?
--- FabiDoctor/FabiDoctor/FabiDoctor/Views & Controllers/Controllers/AlergiesViewController.swift
+++ FabiDoctor/FabiDoctor/FabiDoctor/Views & Controllers/Controllers/AlergiesViewController.swift
@@ -10,8 +10,51 @@ import UIKit
 
 class AlergiesViewController: UIViewController {
 
+    var alergies: [Alergy] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // Setting title of the navigation bar
+        self.navigationItem.title = "Alergias"
+
+        // Setting "add" bar button
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonPressed))
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        self.alergies = AppShared.user.alergies
+//        self.tableView.reloadData()
     }
 
 }
+
+//extension AlergiesViewController: UITableViewDataSource {
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        
+//    }
+//
+//}
+//
+//extension AlergiesViewController: UITableViewDelegate {
+//
+//}
+
+extension AlergiesViewController {
+
+    @objc func addButtonPressed() {
+        self.goToAddAlergyVC()
+    }
+
+    func goToAddAlergyVC() {
+        let viewController = ViewControllerFactory.instantiateVC(ofType: .addAlergy)
+        self.navigationController?.pushViewController(viewController, animated: true)
+    }
+
+}
+
